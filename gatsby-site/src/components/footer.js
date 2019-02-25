ef1089c6ca5654fc71bdf38ce45ddf53e08b8762
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-02-25 15:51:12 -0800

    updates files

---
+++ gatsby-site/src/components/footer.js
@@ -0,0 +1,47 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Footer = ({ siteTitle }) => (
+    <footer 
+        style={{
+            background: `rebeccapurple`,
+            position: `absolute`,
+            bottom: `0`,
+            left: `0`,
+            width: `100%`
+      }}
+    >
+
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+
+    </footer>
+)
+
+Footer.propTypes = {
+    siteTitle: PropTypes.string,
+  }
+  
+Footer.defaultProps = {
+    siteTitle: ``,
+  }
+  
+  export default Footer
