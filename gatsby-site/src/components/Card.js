a8c4eb447d4713174d286548f0e86b8047e058e1
LENA EIVY <applena@gmail.com>
2019-02-26 17:47:44 -0800

    added edit buttons to products and functions for onClick

--- gatsby-site/src/components/Card.js
+++ gatsby-site/src/components/Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import Auth from '../auth/auth';
 
 const customStyles = {
   content: {
@@ -21,6 +22,15 @@ export class Card extends Component {
     this.setState({ modalVisible: visible });
   }
 
+  editItem = () => {
+    //edit the item
+  }
+  
+  deleteItem = () => {
+    //delete the item
+  
+  }
+
 
   render() {
     return (
@@ -39,6 +49,13 @@ export class Card extends Component {
           <img src={this.props.content.image} style={{ width: "500px", height: '500px' }} />
           <div>{this.props.content.description}</div>
           <div>{this.props.content.price}</div>
+          <Auth capibility="update">
+            <button className='editButton' onSubmit={this.editItem(this.props.content.id)}>Edit Product</button>
+          </Auth>
+
+          <Auth capibility="delete">
+            <button className='deleteButton' onSubmit={this.deleteItem(this.props.content.id)}>Delete Product</button>
+          </Auth>
           <button>Add To Cart</button>
           <button onClick={() => this.setModalVisible(!this.state.modalVisible)}>CLOSE!</button>
         </Modal>
