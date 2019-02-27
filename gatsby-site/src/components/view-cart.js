8fd92b2e79c6a6d97d660eed3382c3855c02d76a
Tanner Seramur <seramurt@gmail.com>
2019-02-27 15:30:54 -0800

    paypal button

--- gatsby-site/src/components/view-cart.js
+++ gatsby-site/src/components/view-cart.js
@@ -1,23 +1,33 @@
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import * as actions from '../redux/actions.js';
+import PayPalBtn from './PayPalBtn';
 
 class ViewCart extends Component {
+  state = {
+    isCartCleared: false
+  }
+
   render() {
     console.log('items from redux store', this.props);
     return (
-      <div>
-        <div>hello from view-cart</div>
-       {this.props.cart.map(item=>(
-         <div>{item.name}: {item.id}</div>
-       ))}
-      </div>
+      <React.Fragment>
+        <div>
+          <div>hello from view-cart</div>
+          {this.props.cart.map(item => (
+            <div>{item.name}: {item.id}</div>
+          ))}
+
+        </div>
+        {/* pass card totals, cardlist  */}
+        {/* maybe add clearCart={true} as props */}
+      </React.Fragment>
     )
   }
 }
 
 const mapStateToProps = state => ({
-    cart: state.data.cart
+  cart: state.data.cart
 });
 
 const mapDispatchToProps = (dispatch, getState) => ({
