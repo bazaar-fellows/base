9760cfd0ba6197e7c9067a1f5fbb4dbd1f1c352b
Tanner Seramur <seramurt@gmail.com>
2019-02-27 16:18:47 -0800

    pushhh

--- gatsby-site/src/pages/cart/index.js
+++ gatsby-site/src/pages/cart/index.js
@@ -5,19 +5,17 @@ import { store } from '../index.js';
 import ViewCart from '../../components/view-cart.js';
 import PayPalBtn from '../../components/PayPalBtn';
 
+import '../../components/view-cart.scss'
+
 class Cart extends Component {
   render() {
     console.log('the items in cart :)');
     return (
       <Provider store={store}>
-        <>
-          <div>
-            hello from cart
-      </div>
+        <div className='main-cart-container'>
           <ViewCart />
           <PayPalBtn total={50} />
-
-        </>
+        </div>
       </Provider>
     )
   }
