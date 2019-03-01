98bca41171fe13b60413c19709399a48bd9e79d6
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-02-28 21:00:38 -0800

    adds some about styling

--- gatsby-site/src/pages/lookbook/index.js
+++ gatsby-site/src/pages/lookbook/index.js
@@ -11,7 +11,7 @@ class LookBook extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dogArray: ['https://res.cloudinary.com/olive-atlas/image/upload/v1551400078/Jazz-2.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400366/olive.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400536/crickett.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400669/tiber.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400828/freya.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551401044/Dug.jpg'],
+      dogArray: ['https://res.cloudinary.com/olive-atlas/image/upload/v1551400078/Jazz-2.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400366/olive.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400536/crickett.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400669/tiber.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400828/freya.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551401044/Dug.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551411933/oliveandatlas-35.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551411513/atlas.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551400078/Jazz-2.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551413378/B_L-2.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551413474/Scout.jpg', 'https://res.cloudinary.com/olive-atlas/image/upload/v1551413578/oliveandatlas-12.jpg' ],
       url: '',
     }
   }
